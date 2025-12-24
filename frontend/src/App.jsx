26678cd16b77011b9adc6858c2cdbc436db987a5
paulpart <parthiv.paul5545@gmail.com>
2025-12-24 14:37:58 -0500

    feat: Implement basic routing between Login, Register, and Dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
-import './App.css'
+import {Routes, Route} from 'react-router-dom'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
-  return 
+  return (
+    <div className='main-container'>
+      <Routes>
+          <Route path='/' element={<Dashboard />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
